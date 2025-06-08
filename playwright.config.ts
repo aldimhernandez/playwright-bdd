fd17e5d67f0750a3039de74e24a0051e6e3cc0bd
aldimhernandez <marialdanahz@gmail.com>
2025-06-08 20:09:34 -0300

    feat: :sparkles: add fixture type, file and configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,6 +4,8 @@ import { defineBddConfig, cucumberReporter } from 'playwright-bdd';
 const testDir = defineBddConfig({
   features: 'src/tests/features/**/*.feature',
   steps: 'src/tests/steps/**/*.ts',
+  importTestFrom: "src/fixtures/Fixtures.ts",
+  disableWarnings: { importTestFrom: true },
 });
 
 export default defineConfig({
@@ -15,6 +17,7 @@ export default defineConfig({
     }),
   ],
   use: {
+    baseURL: 'https://playwright.dev',
     screenshot: 'on',
     trace: 'on',
   },
